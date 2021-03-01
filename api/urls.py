79b15aaf824917ec9f968473133688123a2d0c56
Saleh Alammar <hacel@ammasa.net>
2021-03-01 13:28:07 +0300

    retrieve user by username

--- api/urls.py
+++ api/urls.py
@@ -7,6 +7,7 @@ from api import views
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+router.register(r'username', views.RetrieveUserView)
 
 urlpatterns = [
     path("", include(router.urls)),
--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from api.serializers import UserSerializer, GroupSerializer
+from rest_framework import viewsets, mixins
 from rest_framework.permissions import DjangoModelPermissions
-from rest_framework import viewsets
-
+from rest_framework.authentication import TokenAuthentication
 from django.contrib.auth.models import User, Group
 
 
@@ -9,6 +9,12 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [DjangoModelPermissions]
+
+
+class RetrieveUserView(mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = 'username'
 
 
 class GroupViewSet(viewsets.ModelViewSet):
